5c40d7765625d6c5095469c8ef0c55b72e06633a
Rafa <rafaelmejia2005@gmail.com>
2026-05-05 16:02:24 -0600

    Initial Commit

---
+++ MabeAllies/MabeAlliesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MabeAlliesApp.swift
+//  MabeAllies
+//
+//  Created by Rafael on 05/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MabeAlliesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
